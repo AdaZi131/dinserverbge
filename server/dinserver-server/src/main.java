46a7a22f2b2ead190855396f640365b37933a11e
AdaZi131 <adazi@seznam.cz>
2017-04-15 18:54:32 +0200

    Update to java

--- server/dinserver-server/src/main.java
+++ server/dinserver-server/src/main.java
@@ -1,11 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
  *
- * @author adam
+ * @author AdaZi131
  */
 public class main {
 
@@ -15,5 +16,5 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-
+    
 }
